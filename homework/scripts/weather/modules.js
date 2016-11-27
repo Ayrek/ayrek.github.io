616dcfe3a1937ab9c017feb6b57a6629d79b3a4e
Ayrek <kel13005@byui.edu>
2016-11-26 21:44:17 -0700

    Commit message

--- homework/scripts/weather/modules.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    $("#page-header").load("/homework/header.html");
-    $("#page-footer").load("/homework/footer.html");
-});
---
+++ homework/weather_site/scripts/weather/modules.js
@@ -0,0 +1,4 @@
+$(function() {
+    $("#page-header").load("homework/weather_site/header.html");
+    $("#page-footer").load("homework/weather_site/footer.html");
+});
