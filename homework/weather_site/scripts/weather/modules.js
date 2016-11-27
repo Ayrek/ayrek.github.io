db52b78c2d7378ea3a6622ecde0fcd763b0020f3
Ayrek <kel13005@byui.edu>
2016-11-27 00:05:19 -0700

    update

--- homework/weather_site/scripts/weather/modules.js
+++ homework/weather_site/scripts/weather/modules.js
@@ -1,4 +1,4 @@
 $(function() {
-    $("#page-header").load("homework/weather_site/header.html");
-    $("#page-footer").load("homework/weather_site/footer.html");
+    $("#page-header").load("/homework/weather_site/header.html");
+    $("#page-footer").load("/homework/weather_site/footer.html");
 });
