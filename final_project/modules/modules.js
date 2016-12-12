10a3957826ba19eafed74a3e806a052fe6462ebe
Ayrek <kel13005@byui.edu>
2016-12-12 07:44:57 -0700

    update
    final project

---
+++ final_project/modules/modules.js
@@ -0,0 +1,4 @@
+$(function() {
+    $("#page-header").load("/final_project/modules/header.html");
+    $("#page-footer").load("/final_project/modules/footer.html");
+});
--- final_project/scripts/weather/modules.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    $("#page-header").load("/homework/weather_site/header.html");
-    $("#page-footer").load("/homework/weather_site/footer.html");
-});
